2f07ccbcd4e79aad99f25c9e444d062995780f37
romerodevelopments <gandresrp@gmail.com>
2024-06-16 22:51:52 -0400

    feat: create publication model

---
+++ src/app/models/publication.model.ts
@@ -0,0 +1,13 @@
+export interface Publication {
+  id: number;
+  username: string;
+  title: string;
+  content: string;
+  image: string;
+  comments: Comment[];
+}
+
+export interface Comment {
+  username: string;
+  content: string;
+}
