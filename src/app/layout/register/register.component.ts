c0a1e2bfcd2216b618c83ea04f5560fb6e4989b7
romerodevelopments <gandresrp@gmail.com>
2024-06-18 23:24:23 -0400

    feat: create register component

---
+++ src/app/layout/register/register.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { Validators, AbstractControl, ValidationErrors, FormGroup, FormBuilder, AbstractControlOptions } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { MessageDialogComponent } from '../../components/message-dialog/message-dialog.component';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, this.emailExistsValidator.bind(this)]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,18}$/)]],
+      confirmPassword: ['', Validators.required],
+      birthDate: ['', [Validators.required, this.ageValidator]]
+    }, { validators: this.passwordsMatchValidator } as AbstractControlOptions);
+  }
+
+  ageValidator(control: AbstractControl): ValidationErrors | null {
+    const birthDate = new Date(control.value);
+    const age = new Date().getFullYear() - birthDate.getFullYear();
+    if (age < 13) {
+      return { 'tooYoung': true };
+    }
+    return null;
+  }
+
+  passwordsMatchValidator(group: FormGroup): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      group.get('confirmPassword')?.setErrors({ passwordsMismatch: true });
+      return { passwordsMismatch: true };
+    } else {
+      group.get('confirmPassword')?.setErrors(null);
+      return null;
+    }
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const user = this.registerForm.value;
+      this.authService.register(user);
+      this.router.navigate(['/login']);
+      this.dialog.open(MessageDialogComponent, {
+        data: {
+          title: 'Aviso',
+          message: 'Registro realizado con éxito'
+        }
+      });
+    }
+  }
+
+  emailExistsValidator(control: AbstractControl): ValidationErrors | null {
+    if (this.authService.emailExists(control.value)) {
+      return { emailExists: true };
+    }
+    return null;
+  }
+
+  onReset(): void {
+    this.registerForm.reset();
+  }
+}
