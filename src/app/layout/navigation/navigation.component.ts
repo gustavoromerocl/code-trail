3436505b1966e7271b50de9affe9c8ded3b1b1fb
romerodevelopments <gandresrp@gmail.com>
2024-06-24 20:47:16 -0400

    feat: document navigation component

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -11,6 +11,10 @@ import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+/**
+ * Componente de navegación.
+ * Maneja la barra lateral de navegación y la barra de herramientas superior.
+ */
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -27,14 +31,26 @@ import { CommonModule } from '@angular/common';
   ]
 })
 export class NavigationComponent {
+  /**
+   * Observable que indica si la vista actual es en un dispositivo móvil.
+   */
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
+  /**
+   * Constructor del componente NavigationComponent.
+   * @param breakpointObserver Observador de puntos de quiebre para determinar si la vista es en un dispositivo móvil.
+   * @param authService Servicio de autenticación.
+   */
   constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
 
+  /**
+   * Verifica si el usuario actual es un administrador.
+   * @returns Verdadero si el usuario es un administrador, falso en caso contrario.
+   */
   isAdmin(): boolean {
     const currentUser = this.authService.getCurrentUser();
     return currentUser ? this.authService.getUserRole(currentUser) === 'admin' : false;
