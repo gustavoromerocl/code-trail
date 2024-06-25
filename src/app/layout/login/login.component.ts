bd08b8af9d82db3e9728fa39ed5785011f765c79
romerodevelopments <gandresrp@gmail.com>
2024-06-24 20:45:42 -0400

    feat: document login component

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -9,6 +9,10 @@ import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MessageDialogComponent } from '../../components/message-dialog/message-dialog.component';
 
+/**
+ * Componente de login.
+ * Permite a los usuarios ingresar sus credenciales para iniciar sesión en la aplicación.
+ */
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -25,15 +29,28 @@ import { MessageDialogComponent } from '../../components/message-dialog/message-
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  /** El nombre de usuario ingresado por el usuario */
   username: string = '';
+  /** La contraseña ingresada por el usuario */
   password: string = '';
 
+  /**
+   * Constructor del componente LoginComponent.
+   * @param authService Servicio de autenticación
+   * @param router Servicio de enrutamiento
+   * @param dialog Servicio de diálogo
+   */
   constructor(private authService: AuthService, private router: Router, private dialog: MatDialog) { }
 
+  /**
+   * Método de inicio de sesión.
+   * Verifica las credenciales del usuario y redirige al dashboard si son correctas,
+   * de lo contrario, muestra un mensaje de error.
+   */
   login() {
     if (this.authService.login(this.username, this.password)) {
       this.router.navigate(['/dashboard']);
-    } else  {
+    } else {
       this.dialog.open(MessageDialogComponent, {
         data: {
           title: 'Error',
