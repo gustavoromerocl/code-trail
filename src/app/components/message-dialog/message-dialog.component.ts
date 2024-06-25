cd942c608084cd595102f126008fd62f712f4ca1
romerodevelopments <gandresrp@gmail.com>
2024-06-24 20:40:32 -0400

    feat: document message dialog component

--- src/app/components/message-dialog/message-dialog.component.ts
+++ src/app/components/message-dialog/message-dialog.component.ts
@@ -1,19 +1,31 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+/**
+ * Componente de diálogo de mensaje.
+ * Este componente muestra un mensaje en un cuadro de diálogo modal.
+ */
 @Component({
   selector: 'app-message-dialog',
   standalone: true,
   imports: [],
   templateUrl: './message-dialog.component.html',
-  styleUrl: './message-dialog.component.css'
+  styleUrls: ['./message-dialog.component.css']
 })
 export class MessageDialogComponent {
+  /**
+   * Constructor del componente.
+   * @param {MatDialogRef<MessageDialogComponent>} dialogRef - Referencia al cuadro de diálogo.
+   * @param {{ title: string; message: string }} data - Datos inyectados en el cuadro de diálogo, que incluyen el título y el mensaje.
+   */
   constructor(
     private dialogRef: MatDialogRef<MessageDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
   ) {}
 
+  /**
+   * Cierra el cuadro de diálogo.
+   */
   close(): void {
     this.dialogRef.close();
   }
