e6367b227b5eebeb8eb6f9be0da5e5587337bc6e
romerodevelopments <gandresrp@gmail.com>
2024-07-07 14:03:17 -0400

    feat: add http client provider

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient()
   ]
 })
   .catch(err => console.error(err));
